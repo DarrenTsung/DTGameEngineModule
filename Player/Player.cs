8f3777269a4a5a30202ad1483036b58e23baa4a7
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-10-09 10:22:59 -0700

    Added some base code to player to handle primary direction / secondary direction

--- Player/Player.cs
+++ Player/Player.cs
@@ -3,4 +3,21 @@ using System.Collections;
 ﻿using UnityEngine;
 
 public abstract class Player : MonoBehaviour {
+  // PRAGMA MARK - INTERNAL
+  protected void Awake() {
+    this.RegisterNotifications();
+  }
+  
+  protected virtual void RegisterNotifications() {
+    DTNotifications.HandlePrimaryDirection.AddListener(this.HandlePrimaryDirection);
+    DTNotifications.HandleSecondaryDirection.AddListener(this.HandleSecondaryDirection);
+  }
+  
+  protected virtual void HandlePrimaryDirection(Vector2 primaryDirection) {
+    // do nothing
+  }
+  
+  protected virtual void HandleSecondaryDirection(Vector2 secondaryDirection) {
+    // do nothing
+  }
 }
