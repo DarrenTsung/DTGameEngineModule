a07edf9a023f0dbaca76f936e32d75f7e4bf263e
Darren Tsung <darrentsung@Darrens-MacBook-Pro.local>
2015-11-07 08:03:35 -0800

    Base templates for GameManager

---
+++ GameManager/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using DT;
+using System.Collections;
+﻿using UnityEngine;
+
+public class GameManager : Singleton<GameManager> {
+	// PRAGMA MARK - Interface
+	
+	
+	// PRAGMA MARK - Internal
+	protected void Awake() {
+	
+	}
+	
+	protected void Update() {
+	
+	}
+}
