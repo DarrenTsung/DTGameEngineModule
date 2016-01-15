e9ab9ee2c2af815b910817dfc6f264ab0767c479
Darren Tsung <darren.tsung@gmail.com>
2016-01-15 06:11:50 -0800

    Spacing

--- UI/ViewControllers/BasicViewController.cs
+++ UI/ViewControllers/BasicViewController.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 using UnityEngine.Events;
 
 namespace DT {
-  // NOTE: this view controller only handles presenting a single view 
-  public abstract class BasicViewController<TView> : IViewController, IStartShowSubscriber<IView>, 
+  // NOTE: this view controller only handles presenting a single view
+  public abstract class BasicViewController<TView> : IViewController, IStartShowSubscriber<IView>,
                                                                       IEndShowSubscriber<IView>,
                                                                       IStartDismissSubscriber<IView>,
                                                                       IEndDismissSubscriber<IView>
@@ -17,19 +17,19 @@ namespace DT {
         this._view.Show();
       });
     }
-    
+
     public void Dismiss() {
       this.InitializeViewIfNeededAndDo(() => {
         this._view.Dismiss();
       });
     }
-    
-    
+
+
     // PRAGMA MARK - Internal
     protected ShowDismissEvents<IViewController> _showDismissEvents = new ShowDismissEvents<IViewController>();
     protected TView _view;
     protected string _viewPrefabName;
-    
+
     protected void InitializeViewIfNeededAndDo(Action callback) {
       // load the view if uninitialized
       if (this._view == null) {
@@ -40,59 +40,59 @@ namespace DT {
         callback();
       }
     }
-    
+
     protected void InitializeView(Action callback) {
       PrefabLoader.InstantiatePrefab(this._viewPrefabName, (GameObject loadedObject) => {
         this._view = loadedObject.GetComponent<TView>();
         this._view.AddShowDismissEvents(this);
-        
+
         // HACK: make this better Darren
         GameObject[] canvases = GameObject.FindGameObjectsWithTag("Canvas");
-        loadedObject.transform.SetParent(canvases[0].transform);
-        
+        loadedObject.transform.SetParent(canvases[0].transform, false);
+
         IContextContainer contextContainer = this._view as IContextContainer;
         if (contextContainer == null) {
           Debug.LogError("InitializeView: view is not an IContextContainer, probably should be!");
         } else {
           contextContainer.ProvideContext(this);
         }
-        
+
         callback();
       });
     }
-    
-    
+
+
     // PRAGMA MARK - IStartShowSubscriber<IView> implementation
     protected UnityEvent _onEndShowOnce = new UnityEvent();
-    
+
     public void OnStartShow(IView view) {
       this._showDismissEvents.InvokeOnStartShow(this);
     }
-    
+
     // PRAGMA MARK - IEndShowSubscriber<IView> implementation
     public void OnEndShow(IView view) {
       this._onEndShowOnce.Invoke();
       this._onEndShowOnce.RemoveAllListeners();
-      
+
       this._showDismissEvents.InvokeOnEndShow(this);
     }
-    
+
     // PRAGMA MARK - IStartDismissSubscriber<IView> implementation
     public void OnStartDismiss(IView view) {
       this._showDismissEvents.InvokeOnStartDismiss(this);
     }
-    
+
     // PRAGMA MARK - IEndDismissSubscriber<IView> implementation
     public void OnEndDismiss(IView view) {
       this._showDismissEvents.InvokeOnEndDismiss(this);
     }
-    
-    
+
+
     // PRAGMA MARK - IShowDismissEvents implementation
     public void AddShowDismissEvents(object subscriber) {
       this._showDismissEvents.AddShowDismissEvents(subscriber);
     }
-    
+
     public void RemoveShowDismissEvents(object subscriber) {
       this._showDismissEvents.RemoveShowDismissEvents(subscriber);
     }
