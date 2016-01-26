057f9a6e115b9db3270244a66ebfda08abd93589
Darren Tsung <darren.tsung@gmail.com>
2016-01-26 06:17:04 -0800

    Added player modules to further separate out logic inside player

--- Player/Player.cs
+++ Player/Player.cs
@@ -4,49 +4,59 @@ using System.Collections;
 
 namespace DT.GameEngine {
   public class Player : MonoBehaviour {
-    // PRAGMA MARK - Public Interface 
+    // PRAGMA MARK - Public Interface
     public int PlayerIndex {
-      set { 
+      set {
         this.CleanupNotifications();
-        _playerIndex = value; 
+        _playerIndex = value;
         this.RegisterNotifications();
       }
       get { return _playerIndex; }
     }
-    
+
     public void DisableInputAfterDelay(float delay) {
       this.DoAfterDelay(delay, () => { this.DisableInput(); });
     }
-    
+
     public void DisableInputForTime(float time) {
       this.DisableInput();
       this.EnableInputAfterDelay(time);
     }
-    
+
     public void DisableInput() {
       Toolbox.GetInstance<IPlayerInputManager>().SetInputDisabledForPlayer(_playerIndex, true);
     }
-    
+
     public void EnableInputAfterDelay(float delay) {
       this.DoAfterDelay(delay, () => { this.EnableInput(); });
     }
-    
+
     public void EnableInput() {
       Toolbox.GetInstance<IPlayerInputManager>().SetInputDisabledForPlayer(_playerIndex, false);
     }
-    
+
     // PRAGMA MARK - Internal
     [SerializeField, ReadOnly]
     protected int _playerIndex = 0;
-    
+
     protected virtual void Awake() {
       // do nothing
     }
-    
+
     protected void OnDisable() {
       this.CleanupNotifications();
     }
-    
+
+    protected T CreateModule<T>() where T : PlayerModule {
+      GameObject moduleObject = new GameObject();
+      moduleObject.name = typeof(T).Name;
+      moduleObject.transform.SetParent(this.transform);
+
+      T moduleComponent = moduleObject.AddComponent<T>();
+      moduleComponent.SetupWithContext(this);
+      return moduleComponent;
+    }
+
     protected virtual void RegisterNotifications() { }
     protected virtual void CleanupNotifications() { }
   }
---
+++ Player/PlayerModule.cs
@@ -0,0 +1,33 @@
+using DT;
+using System.Collections;
+using UnityEngine;
+
+namespace DT.GameEngine {
+  public class PlayerModule : MonoBehaviour {
+    // PRAGMA MARK - Public Interface
+    public void SetupWithContext(Player context) {
+      this._contextPlayerIndex = context.PlayerIndex;
+      this._contextGameObject = context.gameObject;
+
+      this.InitializeAfterContext();
+    }
+
+
+    // PRAGMA MARK - Unity Lifecycle
+    protected void OnDisable() {
+      this.CleanupNotifications();
+    }
+
+
+    // PRAGMA MARK - Internal
+    protected int _contextPlayerIndex;
+    protected GameObject _contextGameObject;
+
+    protected virtual void InitializeAfterContext() {
+      this.RegisterNotifications();
+    }
+
+    protected virtual void RegisterNotifications() { }
+    protected virtual void CleanupNotifications() { }
+  }
+}
