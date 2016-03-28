c0ee3bed11d7ee2d9230ffbc99bba7b5502963f3
Darren Tsung <darren.tsung@gmail.com>
2016-03-28 09:11:26 -0700

    Switch WeightedSelectionSystem to pass in function get weight from entity, move methods from LootDropGroupManager to LootManager for clarity

--- DTEntityComponentSystem/Shared/Systems/WeightSelectionSystem.cs
+++ DTEntityComponentSystem/Shared/Systems/WeightSelectionSystem.cs
@@ -5,37 +5,22 @@ using UnityEngine;
 
 namespace DT.GameEngine {
   public static class WeightedSelectionSystem {
-    public static T SelectWeightedObject<T>(IEnumerable<T> collection) where T : DTEntity {
-      return null;
-      // WeightedEntityWrapper chosenWrapper = WeightedSelectionUtil.SelectWeightedObject((new WeightedEntityWrapper(entity) for entity in collection));
-      //
-      // if (chosenWrapper == null) {
-      //   return null;
-      // }
-      //
-      // return chosenWrapper.wrappedEntity;
+    // PRAGMA MARK - Static
+    public static TEntity SelectWeightedObject<TEntity>(IEnumerable<TEntity> collection) where TEntity : DTEntity {
+      return WeightedSelectionUtil.SelectWeightedObject(collection, WeightedSelectionSystem.GetWeightForEntity);
     }
 
-    // NOTE (darren): we can pool these if they become a performance issue
-    // for the moment, it seems like unnecessary work
-    private class WeightedEntityWrapper : IWeightedObject {
-      public DTEntity wrappedEntity;
-      private WeightComponent _weightComponent;
-
-      public WeightedEntityWrapper(DTEntity entity) {
-        this.wrappedEntity = entity;
-        this._weightComponent = entity.GetComponent<WeightComponent>();
-        if (this._weightComponent == null) {
-          Debug.LogError("WeightedEntityWrapper - failed to get weight component from entity!");
-        }
+    private static int GetWeightForEntity(DTEntity entity) {
+      if (entity == null) {
+        return 0;
       }
 
-      // PRAGMA MARK - IWeightedObject Implementation
-      public int Weight {
-        get {
-          return this._weightComponent == null ? 0 : this._weightComponent.weight;
-        }
+      WeightComponent weightComponent = entity.GetComponent<WeightComponent>();
+      if (weightComponent == null) {
+        return 0;
       }
+
+      return weightComponent.weight;
     }
   }
 }
--- Loot/LootManager.cs
+++ Loot/LootManager.cs
@@ -1,15 +1,25 @@
 using DT;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace DT.GameEngine {
   public class LootManager : MonoBehaviour {
+    // PRAGMA MARK - Static
+    private static List<LootDrop> GetLootDropsForId(int lootDropGroupId) {
+      LootDropGroup dropGroup = IdList<LootDropGroup>.Instance.LoadById(lootDropGroupId);
+
+      IIdList<LootDrop> lootDropList = ListFactory<LootDrop>.Instance.GetList();
+      return (from lootDropId in dropGroup.lootDropIds select lootDropList.LoadById(lootDropId)).ToList();
+    }
+
+
     // PRAGMA MARK - Public Interface
     public ILootReward[] SelectRewardsForLootDropGroupId(int lootDropGroupId) {
       List<ILootReward> rewards = new List<ILootReward>();
 
-      List<LootDrop> lootDrops = LootDropGroupManager.GetLootDropsForId(lootDropGroupId);
-      LootDrop selectedLootDrop = WeightedSelectionUtil.SelectWeightedObject(lootDrops);
+      List<LootDrop> lootDrops = LootManager.GetLootDropsForId(lootDropGroupId);
+      LootDrop selectedLootDrop = WeightedSelectionSystem.SelectWeightedObject(lootDrops);
 
       if (selectedLootDrop == null) {
         Debug.LogError("SelectRewardsForLootDropGroupId: failed to select lootDrop with group id: " + lootDropGroupId);
