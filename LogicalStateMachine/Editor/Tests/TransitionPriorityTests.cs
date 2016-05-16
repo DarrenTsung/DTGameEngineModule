3edcf8a933caaaec64ec73574133467165e1b84e
Darren Tsung <darren.tsung@gmail.com>
2016-05-16 07:04:59 -0700

    Test what happens when multiple conditions match

---
+++ LogicalStateMachine/Editor/Tests/TransitionPriorityTests.cs
@@ -0,0 +1,47 @@
+using DT;
+using NUnit.Framework;
+using NSubstitute;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace DT.GameEngine {
+  public class TransitionPriorityTests {
+    [Test]
+    public void WhenMultipleMatchingTransitions_TheFirstIsTaken() {
+      Graph graph = ScriptableObject.CreateInstance(typeof(Graph)) as Graph;
+
+      Node nodeA = graph.MakeNode();
+      Node nodeB = graph.MakeNode();
+      Node nodeC = graph.MakeNode();
+
+      Transition bTransition = new Transition();
+      bTransition.AddTransitionCondition(new IntTransitionCondition("Key", 5));
+      NodeTransition bNodeTransition = new NodeTransition { target = nodeB.Id, transition = bTransition };
+      graph.AddOutgoingTransitionForNode(nodeA, bNodeTransition);
+
+      Transition cTransition = new Transition();
+      cTransition.AddTransitionCondition(new IntTransitionCondition("Key", 5));
+      NodeTransition cNodeTransition = new NodeTransition { target = nodeC.Id, transition = cTransition };
+      graph.AddOutgoingTransitionForNode(nodeA, cNodeTransition);
+
+      IGraphContext stubContext = Substitute.For<IGraphContext>();
+      stubContext.HasIntParameterKey(Arg.Is("Key")).Returns(true);
+      stubContext.GetInt(Arg.Is("Key")).Returns(5);
+
+      IGraphContextFactory stubFactory = Substitute.For<IGraphContextFactory>();
+      stubFactory.MakeContext().Returns(stubContext);
+      GraphContextFactoryLocator.Provide(stubFactory);
+
+      bool bEntered = false;
+      nodeB.OnEnter += () => { bEntered = true; };
+      bool cEntered = false;
+      nodeC.OnEnter += () => { cEntered = true; };
+
+      graph.Start();
+      Assert.IsTrue(bEntered);
+      Assert.IsFalse(cEntered);
+    }
+  }
+}
