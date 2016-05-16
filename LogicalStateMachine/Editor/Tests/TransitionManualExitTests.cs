54b538c3b414092c757ea34864d505144d47eb2a
Darren Tsung <darren.tsung@gmail.com>
2016-05-16 06:40:49 -0700

    Check that int transition conditions and wait for manual exit can be layered together

--- LogicalStateMachine/Editor/Tests/TransitionManualExitTests.cs
+++ LogicalStateMachine/Editor/Tests/TransitionManualExitTests.cs
@@ -27,5 +27,37 @@ namespace DT.GameEngine {
       nodeA.TriggerManualExit();
       Assert.IsTrue(entered);
     }
+
+    [TestCase("Key", 5, true)]
+    [TestCase("Key", 3, false)]
+    [TestCase("NotKey", 5, false)]
+    [TestCase("NotKey", 3, false)]
+    public void IntTransitionCondition_WithWaitForManualExit_TakenAfterManualExitTriggered(string key, int targetValue, bool expectedEntered) {
+      Graph graph = ScriptableObject.CreateInstance(typeof(Graph)) as Graph;
+      Node nodeA = graph.MakeNode();
+      Node nodeB = graph.MakeNode();
+
+      Transition transition = new Transition(waitForManualExit: true);
+      transition.AddTransitionCondition(new IntTransitionCondition(key, targetValue));
+      NodeTransition nodeTransition = new NodeTransition { target = nodeB.Id, transition = transition };
+      graph.AddOutgoingTransitionForNode(nodeA, nodeTransition);
+
+      IGraphContext stubContext = Substitute.For<IGraphContext>();
+      stubContext.HasIntParameterKey(Arg.Is("Key")).Returns(true);
+      stubContext.GetInt(Arg.Is("Key")).Returns(5);
+
+      IGraphContextFactory stubFactory = Substitute.For<IGraphContextFactory>();
+      stubFactory.MakeContext().Returns(stubContext);
+      GraphContextFactoryLocator.Provide(stubFactory);
+
+      bool entered = false;
+      nodeB.OnEnter += () => { entered = true; };
+
+      graph.Start();
+      Assert.IsFalse(entered);
+
+      nodeA.TriggerManualExit();
+      Assert.AreEqual(expectedEntered, entered);
+    }
   }
 }
