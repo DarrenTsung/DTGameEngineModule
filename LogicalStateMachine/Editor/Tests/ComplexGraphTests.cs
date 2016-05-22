ec8eb3aafc541dc0289535838398b5a84895cc71
Darren Tsung <darren.tsung@gmail.com>
2016-05-22 08:21:17 -0700

    Add test to make sure that cascading transitions that add active nodes multiple times works correctly

--- LogicalStateMachine/Editor/Tests/ComplexGraphTests.cs
+++ LogicalStateMachine/Editor/Tests/ComplexGraphTests.cs
@@ -105,5 +105,51 @@ namespace DT.GameEngine {
       Assert.IsFalse(cEntered);
       Assert.IsTrue(dEntered);
     }
+
+    [Test]
+    public void Entering_TheSameNode_MultipleTimesWithCascade_Works() {
+      Graph graph = new Graph();
+
+      // A -> B <-> C
+      Node nodeA = graph.MakeNode();
+      Node nodeB = graph.MakeNode();
+      Node nodeC = graph.MakeNode();
+
+      Transition abTransition = new Transition();
+      NodeTransition abNodeTransition = new NodeTransition { targets = new NodeId[] { nodeB.Id }, transition = abTransition };
+      graph.AddOutgoingTransitionForNode(nodeA, abNodeTransition);
+
+      Transition bcTransition = new Transition();
+      bcTransition.AddTransitionCondition(new TriggerTransitionCondition("BGoToC"));
+      NodeTransition bcNodeTransition = new NodeTransition { targets = new NodeId[] { nodeC.Id }, transition = bcTransition };
+      graph.AddOutgoingTransitionForNode(nodeB, bcNodeTransition);
+
+      Transition cbTransition = new Transition();
+      cbTransition.AddTransitionCondition(new TriggerTransitionCondition("CGoToB"));
+      NodeTransition cbNodeTransition = new NodeTransition { targets = new NodeId[] { nodeB.Id }, transition = cbTransition };
+      graph.AddOutgoingTransitionForNode(nodeC, cbNodeTransition);
+
+      IGraphContext stubContext = Substitute.For<IGraphContext>();
+      stubContext.HasTriggerParameterKey(Arg.Is("BGoToC")).Returns(true);
+      stubContext.HasTriggerParameterKey(Arg.Is("CGoToB")).Returns(true);
+      // returns true then false
+      stubContext.HasTrigger(Arg.Is("BGoToC")).Returns(true, false);
+      // returns true then false
+      stubContext.HasTrigger(Arg.Is("CGoToB")).Returns(true, false);
+
+      IGraphContextFactory stubFactory = Substitute.For<IGraphContextFactory>();
+      stubFactory.MakeContext().Returns(stubContext);
+      GraphContextFactoryLocator.Provide(stubFactory);
+
+      int bEnteredCount = 0;
+      nodeB.OnEnter += () => { bEnteredCount++; };
+      int cEnteredCount = 0;
+      nodeC.OnEnter += () => { cEnteredCount++; };
+
+      graph.Start();
+
+      Assert.AreEqual(2, bEnteredCount);
+      Assert.AreEqual(1, cEnteredCount);
+    }
   }
 }
