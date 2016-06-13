3acab0484a8feec2fb6150f5a5b25a6db7b4700d
Darren Tsung <darren.tsung@gmail.com>
2016-06-13 09:24:42 -0700

    Add extensions for IdQuantity to remove from UserInventory and use inference to reduce number of code changes when switching different types of entities to charge

--- IdEntities/IdQuantity/IdQuantityExtensions.cs
+++ IdEntities/IdQuantity/IdQuantityExtensions.cs
@@ -9,5 +9,29 @@ namespace DT.GameEngine {
     public static TEntity GetEntity<TEntity>(this IdQuantity<TEntity> idQuantity) where TEntity : DTEntity {
       return IdList<TEntity>.Instance.LoadById(idQuantity.id);
     }
+
+    public static bool CanRemoveFromUserInventory<TEntity>(this IEnumerable<IdQuantity<TEntity>> idQuantityList) where TEntity : DTEntity {
+      return UserIdInventory<TEntity>.Instance.CanRemoveIdQuantityList(idQuantityList);
+    }
+
+    public static bool CanRemoveFromUserInventory<TEntity>(this IdQuantity<TEntity> idQuantity) where TEntity : DTEntity {
+      return UserIdInventory<TEntity>.Instance.CanRemoveIdQuantity(idQuantity);
+    }
+
+    public static void RemoveFromUserInventory<TEntity>(this IEnumerable<IdQuantity<TEntity>> idQuantityList) where TEntity : DTEntity {
+      UserIdInventory<TEntity>.Instance.RemoveIdQuantityList(idQuantityList);
+    }
+
+    public static void RemoveFromUserInventory<TEntity>(this IdQuantity<TEntity> idQuantity) where TEntity : DTEntity {
+      UserIdInventory<TEntity>.Instance.RemoveIdQuantity(idQuantity);
+    }
+
+    public static void AddToUserInventory<TEntity>(this IEnumerable<IdQuantity<TEntity>> idQuantityList) where TEntity : DTEntity {
+      UserIdInventory<TEntity>.Instance.AddIdQuantityList(idQuantityList);
+    }
+
+    public static void AddToUserInventory<TEntity>(this IdQuantity<TEntity> idQuantity) where TEntity : DTEntity {
+      UserIdInventory<TEntity>.Instance.AddIdQuantity(idQuantity);
+    }
 	}
 }
