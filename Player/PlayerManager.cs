f180e106b0e233643ab9c1602fd0b22cd07cdd1e
Darren Tsung <darren.tsung@gmail.com>
2016-06-15 10:39:42 -0700

    Don't destroy player if previous player is new player

--- Player/PlayerManager.cs
+++ Player/PlayerManager.cs
@@ -7,33 +7,33 @@ using System.Linq;
 namespace DT.GameEngine {
 	public class PlayerManager : MonoBehaviour {
 		protected PlayerManager() {}
-		
+
 		// PRAGMA MARK - Public Interface
 		public GameObject[] Players {
 			get {
-				return _players.Values.ToArray();
+				return this._players.Values.ToArray();
 			}
 		}
-		
+
 		public void SetPlayer(int playerIndex, GameObject player) {
-			GameObject previousPlayer = _players.SafeGet(playerIndex, null);
-			if (previousPlayer != null) {
-				Destroy(previousPlayer);
+			GameObject previousPlayer = this._players.SafeGet(playerIndex, null);
+			if (previousPlayer != null && previousPlayer != player) {
+				GameObject.Destroy(previousPlayer);
 			}
-			_players[playerIndex] = player;
-			
+			this._players[playerIndex] = player;
+
 			Player playerComponent = player.GetComponent<Player>();
 			playerComponent.PlayerIndex = playerIndex;
-			
+
 			DTGameEngineNotifications.PlayerChanged.Invoke(playerIndex, player);
 		}
-		
+
 		// PRAGMA MARK - Internal
 		[Header("---- PlayerManager Properties ----")]
 		protected Dictionary<int, GameObject> _players;
-		
+
 		protected virtual void Awake() {
-			_players = new Dictionary<int, GameObject>();
+			this._players = new Dictionary<int, GameObject>();
 		}
 	}
 }
