545db8d6cf1c9613f514c77f0a92e9594f52ed4f
Darren Tsung <darren.tsung@gmail.com>
2016-06-30 09:32:06 -0700

    Added a flying entity quantity + flying entity target

--- ReusableUIEffects/FlyingEntityQuantity/FlyingEntityQuantity.cs
+++ ReusableUIEffects/FlyingEntityQuantity/FlyingEntityQuantity.cs
@@ -3,16 +3,59 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace DT.GameEngine {
   public class FlyingEntityQuantity : MonoBehaviour {
+    // PRAGMA MARK - Static Public Interface
+    public static float kAnimationDuration = 2.0f;
+
+    public static void Make(EntityQuantity entityQuantity, Vector2 screenPosition, Action finishedCallback) {
+      FlyingEntityQuantity f = ObjectPoolManager.Instantiate<FlyingEntityQuantity>("FlyingEntityQuantity");
+      CanvasUtil.ParentUIElementToCanvas(f.gameObject, CanvasUtil.ScreenSpaceMainCanvas);
+
+      f.transform.position = screenPosition;
+      f.Configure(entityQuantity, finishedCallback);
+    }
+
+
     // PRAGMA MARK - Public Interface
-    public void Configure(EntityQuantity entityQuantity) {
+    public void Configure(EntityQuantity entityQuantity, Action finishedCallback) {
       this._entityQuantity = entityQuantity;
+
+      string key = FlyingEntityTarget.KeyForTypeId(entityQuantity.entityType, entityQuantity.entity.Id());
+      RectTransform targetTransform = FlyingEntityTarget.targets.GetValue(key);
+      if (targetTransform == null) {
+        Debug.LogError("Failed to find target for key: " + key + "!");
+        ObjectPoolManager.Recycle(this.gameObject);
+        return;
+      }
+
+      this._displayImage.sprite = this._entityQuantity.entity.DisplaySprite();
+      this._quantityText.Text = this._entityQuantity.quantity.ToString();
+
+      this.AnimateToTarget(targetTransform.position, finishedCallback);
     }
 
 
     // PRAGMA MARK - Internal
+    [Header("Outlets")]
+    [SerializeField] private Image _displayImage;
+    [SerializeField] private TextOutlet _quantityText;
+
     private EntityQuantity _entityQuantity;
+
+    private void AnimateToTarget(Vector2 targetPosition, Action callback) {
+      Vector2 startPosition = this.transform.position;
+
+      CubicBezierV2 bezier = new CubicBezierV2(startPosition, targetPosition);
+      this.DoEaseEveryFrameForDuration(FlyingEntityQuantity.kAnimationDuration, EaseType.SineIn, (float percentage) => {
+        Vector2 position = bezier.Evaluate(percentage);
+        this.transform.position = position;
+      }, finishedCallback: () => {
+        ObjectPoolManager.Recycle(this.gameObject);
+        callback.Invoke();
+      });
+    }
 	}
 }
--- ReusableUIEffects/FlyingEntityQuantity/FlyingEntityTarget.cs
+++ ReusableUIEffects/FlyingEntityQuantity/FlyingEntityTarget.cs
@@ -6,7 +6,20 @@ using System.Collections.Generic;
 
 namespace DT.GameEngine {
   public class FlyingEntityTarget : MonoBehaviour {
+    // PRAGMA MARK - Static Public Interface
+    public static StringRegistry<RectTransform> targets = new StringRegistry<RectTransform>();
+
+    public static string KeyForTypeId(Type entityType, int id) {
+      return string.Format("FlyingEntityTarget{0}{1}", entityType.Name, id);
+    }
+
+
     // PRAGMA MARK - Internal
     [SerializeField] private EntityTypeId _entityTypeId;
+
+    void Awake() {
+      string key = FlyingEntityTarget.KeyForTypeId(this._entityTypeId.EntityType(), this._entityTypeId.id);
+      FlyingEntityTarget.targets.Register(key, this.GetRequiredComponent<RectTransform>());
+    }
 	}
 }
