1ef8bb9fbc2688016793acdf380ac01b95d25c3e
Darren Tsung <darren.tsung@gmail.com>
2016-06-30 09:57:53 -0700

    Attach flying entity quantity to a parent recyclable, invoke finished callback on recycle if it hasn't been invoked already

--- ReusableUIEffects/FlyingEntityQuantity/FlyingEntityQuantity.cs
+++ ReusableUIEffects/FlyingEntityQuantity/FlyingEntityQuantity.cs
@@ -6,13 +6,21 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 
 namespace DT.GameEngine {
-  public class FlyingEntityQuantity : MonoBehaviour {
+  public class FlyingEntityQuantity : MonoBehaviour, IRecycleCleanupSubscriber {
     // PRAGMA MARK - Static Public Interface
     public static float kAnimationDuration = 2.0f;
 
-    public static void Make(EntityQuantity entityQuantity, Vector2 screenPosition, Action finishedCallback) {
+    public static void Make(EntityQuantity entityQuantity, Vector2 screenPosition, GameObject recycleParent, Action finishedCallback) {
+      RecyclablePrefab parentRecyclable = recycleParent.GetRequiredComponentInParent<RecyclablePrefab>();
+      if (parentRecyclable == null) {
+        // don't need to log since using required component
+        return;
+      }
+
       FlyingEntityQuantity f = ObjectPoolManager.Instantiate<FlyingEntityQuantity>("FlyingEntityQuantity");
       CanvasUtil.ParentUIElementToCanvas(f.gameObject, CanvasUtil.ScreenSpaceMainCanvas);
+
+      parentRecyclable.AttachChildRecyclableObject(f.gameObject);
 
       f.transform.position = screenPosition;
       f.Configure(entityQuantity, finishedCallback);
@@ -22,6 +30,8 @@ namespace DT.GameEngine {
     // PRAGMA MARK - Public Interface
     public void Configure(EntityQuantity entityQuantity, Action finishedCallback) {
       this._entityQuantity = entityQuantity;
+      this._finishedCallback = finishedCallback;
+      this._finishedCallbackInvoked = false;
 
       string key = FlyingEntityTarget.KeyForTypeId(entityQuantity.entityType, entityQuantity.entity.Id());
       RectTransform targetTransform = FlyingEntityTarget.targets.GetValue(key);
@@ -34,7 +44,13 @@ namespace DT.GameEngine {
       this._displayImage.sprite = this._entityQuantity.entity.DisplaySprite();
       this._quantityText.Text = this._entityQuantity.quantity.ToString();
 
-      this.AnimateToTarget(targetTransform.position, finishedCallback);
+      this.AnimateToTarget(targetTransform.position);
+    }
+
+
+    // PRAGMA MARK - IRecycleCleanupSubscriber Implementation
+    public void OnRecycleCleanup() {
+      this.InvokeFinishedCallbackIfNecessary();
     }
 
 
@@ -43,9 +59,12 @@ namespace DT.GameEngine {
     [SerializeField] private Image _displayImage;
     [SerializeField] private TextOutlet _quantityText;
 
+    private Action _finishedCallback;
+    private bool _finishedCallbackInvoked = false;
+
     private EntityQuantity _entityQuantity;
 
-    private void AnimateToTarget(Vector2 targetPosition, Action callback) {
+    private void AnimateToTarget(Vector2 targetPosition) {
       Vector2 startPosition = this.transform.position;
 
       CubicBezierV2 bezier = new CubicBezierV2(startPosition, targetPosition);
@@ -53,9 +72,16 @@ namespace DT.GameEngine {
         Vector2 position = bezier.Evaluate(percentage);
         this.transform.position = position;
       }, finishedCallback: () => {
+        this.InvokeFinishedCallbackIfNecessary();
         ObjectPoolManager.Recycle(this.gameObject);
-        callback.Invoke();
       });
+    }
+
+    private void InvokeFinishedCallbackIfNecessary() {
+      if (!this._finishedCallbackInvoked) {
+        this._finishedCallback.Invoke();
+        this._finishedCallbackInvoked = true;
+      }
     }
 	}
 }
